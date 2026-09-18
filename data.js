2f2a657b21f372cc331ea629198aed61cc16c15d
RIG360 <134787168+RIG360@users.noreply.github.com>
2026-09-18 14:05:57 +0200

    commit

---
+++ data.js
@@ -0,0 +1,351 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-breit27-3-43_hausflur",
+      "name": "Breit27-3-43_Hausflur",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.382590441292729,
+          "pitch": -0.043757387589769436,
+          "rotation": 0,
+          "target": "1-breit27-3-43_vorraum"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-breit27-3-43_vorraum",
+      "name": "Breit27-3-43_Vorraum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6392196661597129,
+          "pitch": 0.041587574913870995,
+          "rotation": 0,
+          "target": "0-breit27-3-43_hausflur"
+        },
+        {
+          "yaw": 2.2861707296645015,
+          "pitch": -0.05955088345662318,
+          "rotation": 0,
+          "target": "2-breit27-3-43_kuche"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-breit27-3-43_kuche",
+      "name": "Breit27-3-43_Küche",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9087496206556303,
+          "pitch": 0.07108803949235032,
+          "rotation": 0,
+          "target": "1-breit27-3-43_vorraum"
+        },
+        {
+          "yaw": 1.2096618150834146,
+          "pitch": 0.05533524558928349,
+          "rotation": 0,
+          "target": "3-breit27-3-43_kabinett"
+        },
+        {
+          "yaw": -1.9051957798811578,
+          "pitch": 0.018331738889994398,
+          "rotation": 0,
+          "target": "4-breit27-3-43_bad"
+        },
+        {
+          "yaw": 3.0990127917608135,
+          "pitch": 0.003291121247151807,
+          "rotation": 0,
+          "target": "6-breit27-3-43_zimmer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-breit27-3-43_kabinett",
+      "name": "Breit27-3-43_Kabinett",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5279841763782613,
+          "pitch": 0.07547786010101731,
+          "rotation": 0,
+          "target": "2-breit27-3-43_kuche"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-breit27-3-43_bad",
+      "name": "Breit27-3-43_Bad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2291038616900778,
+          "pitch": 0.04059083293026333,
+          "rotation": 0,
+          "target": "5-breit27-3-43_wc"
+        },
+        {
+          "yaw": 1.1971077404507664,
+          "pitch": 0.013754162948526272,
+          "rotation": 0,
+          "target": "2-breit27-3-43_kuche"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-breit27-3-43_wc",
+      "name": "Breit27-3-43_WC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.820293676641774,
+          "pitch": 0.06653027996174643,
+          "rotation": 0,
+          "target": "4-breit27-3-43_bad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-breit27-3-43_zimmer",
+      "name": "Breit27-3-43_Zimmer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22505289770026593,
+          "pitch": 0.03186887876351996,
+          "rotation": 0,
+          "target": "2-breit27-3-43_kuche"
+        },
+        {
+          "yaw": -2.7581007595107963,
+          "pitch": -0.046184292802884386,
+          "rotation": 0,
+          "target": "7-breit27-3-43_fassade"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-breit27-3-43_fassade",
+      "name": "Breit27-3-43_Fassade",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6692355821412406,
+          "pitch": -0.7312050789089461,
+          "rotation": 0,
+          "target": "6-breit27-3-43_zimmer"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Breit27-3-43_260916",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
